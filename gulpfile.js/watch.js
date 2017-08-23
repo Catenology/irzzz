468a5db7f446c1e469861d3ddefa1cb8d421c102
Cherry Wang <chryw@catenology.com>
2017-08-22 23:10:40 -0700

    finished nav; added portfolio page style

--- gulpfile.js/watch.js
+++ gulpfile.js/watch.js
@@ -5,8 +5,6 @@ gulp.task('watch', ['styles'], () => {
   browserSync.init({
     server: './dist',
   });
-  gulp.watch('src/**/*.scss', ['styles']);
-  gulp.watch('src/**/*.js', ['scripts']);
-  gulp.watch('src/**/*.+(md|html)', ['build']);
+  gulp.watch('src/**/*.+(scss|js|md|html)', ['build']);
   gulp.watch('dist').on('change', browserSync.reload);
 });
